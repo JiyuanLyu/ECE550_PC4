d572fab412fb3e117a4e24517ebb3a69cc1c5ef4
Chelsea Lyu <97059580+JiyuanLyu@users.noreply.github.com>
2023-11-09 14:47:06 -0500

    Update sx.v

--- sx.v
+++ sx.v
@@ -3,7 +3,7 @@ module sx (out, a);
     output [31:0] out;
 
     wire [14:0] extend;
-    assign extend = a[16] ? 15'hffff : 15'b0;
+    assign extend = a[16] ? 15'b111111111111111 : 15'b0;
     assign out[16:0] = a[16:0];
     assign out[31:17] = extend[14:0];
 endmodule
