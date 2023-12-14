03de47291c67066b919406b178ba1f319f4917a9
Chelsea Lyu <97059580+JiyuanLyu@users.noreply.github.com>
2023-12-14 01:59:03 -0500

    Delete PC_5 directory

--- PC_5/sx.v
+++
@@ -1,9 +0,0 @@
-module sx (out, a);
-    input [16:0] a;
-    output [31:0] out;
-
-    wire [14:0] extend;
-    assign extend = a[16] ? 15'b111111111111111 : 15'b0;
-    assign out[16:0] = a[16:0];
-    assign out[31:17] = extend[14:0];
-endmodule
